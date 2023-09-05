c9a6599e0c104aa69565e15d866aadfeed19353e
Tarashrita Ganguly <106442772+Tarashrita@users.noreply.github.com>
2023-09-05 17:53:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+NoseX = 0;
+NoseY = 0;
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -10,11 +15,22 @@ function setup() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
-    };
+        NoseX = results[0].pose.nose.x;
+        NoseY = results[0].pose.nose.y;
+        console.log("NoseX = "+NoseX+"NoseY="+NoseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = "+leftWristX+"rightWristX"+rightWristX+"difference"+difference);
+    }
 }
 
 function draw() {
+    document.getElementById("square_side").innerHTML = "width and height of the square is = "+difference+"px";
     background('#F08080');
+    fill("#C0E6F0")
+    stroke("#C0E6F0")
+    square(NoseX,NoseY,difference)
 }
 
 function modelLoaded() {
